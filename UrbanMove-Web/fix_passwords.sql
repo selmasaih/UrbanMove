3a21182ea0d30c7d230257efb429ca99d01fa956
Salma Saih <saihs1425@gmail.com>
2026-05-04 05:37:09 +0100

    Clean up repository: Keep only UrbanMove app and Backend

--- UrbanMove-Web/fix_passwords.sql
+++
@@ -1,3 +0,0 @@
-USE urbanmove;
-UPDATE users SET password_hash = '$2a$12$pWxJmWU3aGVRAlESq.wkze2VIuJhzC7FiTSkTs01aEi1K1YBdDKGa' WHERE email = 'demo@urbanmove.ma';
-UPDATE users SET password_hash = '$2a$12$23eVXFtXOQuDJcFFjK5/.uPyLqxJcDs0etCUaUOg0cyZg7DIiEaRa' WHERE email = 'admin@urbanmove.ma';
